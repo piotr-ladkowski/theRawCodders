d7b6a01980dd58c30869bfeb9890f47cfd6c9dfc
Piotr Łądkowski <piolad2003@gmail.com>
2026-02-23 20:27:55 +0100

    refactor: remove unused data import from DashboardExample component

--- The RawCodders/src/components/dashboard/DashboardExample.tsx
+++ The RawCodders/src/components/dashboard/DashboardExample.tsx
@@ -2,7 +2,6 @@ import { TransactionDashboard } from "@/components/chart-area-interactive"
 //import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
 
-import data from "./data.json"
 
 const DashboardExample = () => {
     return (
